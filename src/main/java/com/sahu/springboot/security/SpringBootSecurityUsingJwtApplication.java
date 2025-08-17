557d9d54e32285c178f054337a9b2ec355cca8f0
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-17 18:29:28 +0530

    initial Project Setup

---
+++ src/main/java/com/sahu/springboot/security/SpringBootSecurityUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUsingJwtApplication.class, args);
+	}
+
+}
